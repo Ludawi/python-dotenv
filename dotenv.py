ba629cb2cf777ab8603464000b9f557f0fc6754c
Ludawi <wistuba9@gmail.com>
2023-09-21 18:02:54 +0200

    initial commit

---
+++ dotenv.py
@@ -0,0 +1,24 @@
+class GetEnv():
+    def __init__(self, key:str):
+        self.key = key
+
+        lines = []
+
+        with open('.env', "r") as file:
+            for line in file:
+                line = line.rstrip()
+                lines.append(line)
+
+
+        def create_lookup(env_arr):
+            lookup_table = {}
+            for item in env_arr:
+                separator = item.index('=')
+                var = item[:separator]
+                value = item[separator+1:]
+                value = value.replace("'",'')
+
+                lookup_table[var] = value
+            print(lookup_table)
+            return lookup_table
+        create_lookup(lines)
