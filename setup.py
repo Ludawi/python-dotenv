9b2b2a8b099db6c38a05e0f6ec2c385c1a3db991
Ludawi <wistuba9@gmail.com>
2023-09-29 17:06:01 +0200

    preparations for v0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'simple_dotenv',
   packages = ['simple_dotenv'],
-  version = '0.1.3',
+  version = '0.1.4',
   license='MIT',
   description = 'simple retrieval of environment vars in .env files',
   author = 'Ludawi',
